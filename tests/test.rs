0bf4503987f5391e67c1c31a873058a46c75540f
Nadav Ivgi <nadav@shesek.info>
2019-03-14 06:50:24 +0200

    Expand tests

--- tests/test.rs
+++ tests/test.rs
@@ -3,7 +3,7 @@ extern crate serde_json;
 
 use serde_json::Value;
 
-use std::ffi::CStr;
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
 #[repr(C)]
@@ -11,19 +11,37 @@ pub struct GA_json {
     _private: [u8; 0],
 }
 
+#[repr(C)]
+pub struct GA_session {
+    _private: [u8; 0],
+}
+
+#[repr(C)]
+pub struct GA_auth_handler {
+    _private: [u8; 0],
+}
+
 #[link(name = "gdk_rpc")]
 extern "C" {
     fn GA_get_networks(ret: *mut *const GA_json);
     fn GA_convert_json_to_string(json: *const GA_json, ret: *mut *const c_char);
+    fn GA_create_session(ret: *mut *mut GA_session);
+    fn GA_connect(sess: *mut GA_session, network: *const c_char, log_level: u32);
 }
 
 #[test]
 fn main() {
-    let mut nets: *const GA_json = std::ptr::null_mut();
     unsafe {
+        let mut nets: *const GA_json = std::ptr::null_mut();
         GA_get_networks(&mut nets);
+        println!("networks: {:#?}\n", json_obj(nets));
+
+        let mut sess: *mut GA_session = std::ptr::null_mut();
+        GA_create_session(&mut sess);
+
+        let network = CString::new("mainnet").unwrap();
+        GA_connect(sess, network.as_ptr(), 5);
     }
-    println!("networks: {:#?}\n", json_obj(nets))
 }
 
 fn json_obj(json: *const GA_json) -> Value {
