b7bd6f964fdc22617f070b2f8988062267c23853
Nadav Ivgi <nadav@shesek.info>
2019-03-14 05:58:08 +0200

    Read strings out properly in test.c

--- test.c
+++ test.c
@@ -4,13 +4,15 @@
 int main (void) {
   GA_json* j;
   GA_get_networks(&j);
-  char result[1000];
+  char* result;
   GA_convert_json_to_string(j, &result);
   printf("result: %s\n\n\n", result);
 
   GA_destroy_json(j);
-  GA_convert_json_to_string(j, &result);
-  printf("result2: %s\n\n\n", result);
+
+  char* result0;
+  GA_convert_json_to_string(j, &result0);
+  printf("result2: %s\n\n\n", result0);
 
   GA_json* j2;
   char jsonstr[] = "{\"hello\":123,\"world\":99999}";
@@ -20,8 +22,8 @@ int main (void) {
   GA_convert_json_to_string(j2, &result);
   printf("result3: %s\n\n\n", result);
 
-  char result2[50];
-  GA_convert_json_value_to_string(j2, "hello", result2);
+  char* result2;
+  GA_convert_json_value_to_string(j2, "hello", &result2);
   printf("result4: %s\n\n\n", result2);
 
   uint32_t result3;
