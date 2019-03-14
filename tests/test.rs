316ce979f7c0acc883605bdba50cc100e83f3c42
Nadav Ivgi <nadav@shesek.info>
2019-03-14 06:25:09 +0200

    tests: Add utility method to transform a GA_json to a serde Value

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,7 @@
 extern crate libc;
+extern crate serde_json;
+
+use serde_json::Value;
 
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
@@ -13,16 +16,14 @@ extern "C" {
 
 #[test]
 fn main() {
-    unsafe {
-        let mut nets: *const GA_json = std::ptr::null_mut();
-        GA_get_networks(&mut nets);
-        print_json("networks", nets);
-    }
+    let mut nets: *const GA_json = std::ptr::null_mut();
+    unsafe { GA_get_networks(&mut nets); }
+    println!("networks: {:#?}\n", json_obj(nets))
 }
 
-fn print_json(desc: &str, json: *const GA_json) {
+fn json_obj(json: *const GA_json) -> Value {
     let mut s: *const c_char = std::ptr::null_mut();
     unsafe { GA_convert_json_to_string(json, &mut s) };
     let s = unsafe { CStr::from_ptr(s) }.to_str().unwrap();
-    println!("{}: {}\n", desc, s);
+    serde_json::from_str(&s).unwrap()
 }
