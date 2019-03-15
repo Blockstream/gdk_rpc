f2098982cdd48a529a8109a7a3457787a1887156
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-15 23:01:27 +0100

    use more reasonable ndk targets

--- ndk.sh
+++ ndk.sh
@@ -2,13 +2,13 @@
 set -eo pipefail
 
 export AR_i686_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android-ar
-export CC_i686_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android28-clang
+export CC_i686_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android19-clang
 export AR_x86_64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android-ar
-export CC_x86_64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android28-clang
+export CC_x86_64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android21-clang
 export AR_armv7_linux_androideabi=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/arm-linux-androideabi-ar
-export CC_armv7_linux_androideabi=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi28-clang
+export CC_armv7_linux_androideabi=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi19-clang
 export AR_aarch64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
-export CC_aarch64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang
+export CC_aarch64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang
 
 source /root/.cargo/env
 
