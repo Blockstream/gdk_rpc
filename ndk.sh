f21f182e3ee8607c4da7f84c8874dedc52c11ff7
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-16 00:30:46 +0100

    use env var ANDROID_NDK to build ndk

--- ndk.sh
+++ ndk.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -eo pipefail
-
-export PATH=${PATH}:/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin
+if [ -f /.dockerenv ]; then
+    source /root/.cargo/env
+    export ANDROID_NDK=/android-ndk-r19b
+fi
+export PATH=${PATH}:${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64/bin
 
 export CC_i686_linux_android=i686-linux-android19-clang
 export CC_x86_64_linux_android=x86_64-linux-android21-clang
 export CC_armv7_linux_androideabi=armv7a-linux-androideabi19-clang
 export CC_aarch64_linux_android=aarch64-linux-android21-clang
-
-if [ -f /.dockerenv ]; then
-    source /root/.cargo/env
-fi
 
 cp cargo-config.toml ~/.cargo/config
 
