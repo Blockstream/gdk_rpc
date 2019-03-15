3f66132a9548c24312ccf714650a93b21f0ce457
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-15 23:08:01 +0100

    simplify config/reusable local

--- ndk.sh
+++ ndk.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-export AR_i686_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android-ar
-export CC_i686_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android19-clang
-export AR_x86_64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android-ar
-export CC_x86_64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android21-clang
-export AR_armv7_linux_androideabi=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/arm-linux-androideabi-ar
-export CC_armv7_linux_androideabi=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi19-clang
-export AR_aarch64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
-export CC_aarch64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android21-clang
+export PATH=${PATH}:/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin
+
+export AR_i686_linux_android=i686-linux-android-ar
+export CC_i686_linux_android=i686-linux-android19-clang
+export AR_x86_64_linux_android=x86_64-linux-android-ar
+export CC_x86_64_linux_android=x86_64-linux-android21-clang
+export AR_armv7_linux_androideabi=arm-linux-androideabi-ar
+export CC_armv7_linux_androideabi=armv7a-linux-androideabi19-clang
+export AR_aarch64_linux_android=aarch64-linux-android-ar
+export CC_aarch64_linux_android=aarch64-linux-android21-clang
 
 source /root/.cargo/env
 
