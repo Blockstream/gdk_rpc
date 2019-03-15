242e719fbcaaff072dccdca6c8fe55778f3c0c04
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-15 19:32:06 +0100

    add ndk build

---
+++ ndk.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -eo pipefail
+
+export AR_i686_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android-ar
+export CC_i686_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/i686-linux-android28-clang
+export AR_x86_64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android-ar
+export CC_x86_64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/x86_64-linux-android28-clang
+export AR_armv7_linux_androideabi=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/arm-linux-androideabi-ar
+export CC_armv7_linux_androideabi=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/armv7a-linux-androideabi28-clang
+export AR_aarch64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android-ar
+export CC_aarch64_linux_android=/android-ndk-r19b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang
+
+source /root/.cargo/env
+
+cp cargo-config.toml ~/.cargo/config
+
+cargo build --target i686-linux-android --release
+cargo build --target x86_64-linux-android --release
+cargo build --target armv7-linux-androideabi --release
+cargo build --target aarch64-linux-android --release
--- stretch_deps.sh
+++ stretch_deps.sh
@@ -4,12 +4,19 @@ set -eo pipefail
 apt update -qq
 apt upgrade --no-install-recommends -yqq
 
-apt install --no-install-recommends -yqq build-essential clang curl ca-certificates
+apt install --no-install-recommends -yqq build-essential clang curl ca-certificates unzip
+
+SHA256SUM_NDK=0fbb1645d0f1de4dde90a4ff79ca5ec4899c835e729d692f433fda501623257a
+curl -sL -o ndk.zip https://dl.google.com/android/repository/android-ndk-r19b-linux-x86_64.zip
+echo "${SHA256SUM_NDK}  ndk.zip" | sha256sum --check
+unzip ndk.zip
+rm ndk.zip
 
 curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain 1.33.0
 
 source /root/.cargo/env
 rustup component add rustfmt clippy
+rustup target add aarch64-linux-android armv7-linux-androideabi i686-linux-android x86_64-linux-android
 
 SHA256SUM_BITCOINCORE=53ffca45809127c9ba33ce0080558634101ec49de5224b2998c489b6d0fc2b17
 curl -sL -o bitcoin.tar.gz https://bitcoincore.org/bin/bitcoin-core-0.17.1/bitcoin-0.17.1-x86_64-linux-gnu.tar.gz \
@@ -25,7 +32,9 @@ curl -sL -o liquid.tar.gz https://github.com/Blockstream/liquid/releases/downloa
  && ln -s liquid-3.14.1.23 liquid \
  && rm liquid.tar.gz
 
+
 if [ -f /.dockerenv ]; then
+    apt remove --purge unzip -yqq
     apt -yqq autoremove
     apt -yqq clean
     rm -rf /var/lib/apt/lists/* /var/cache/* /tmp/* /usr/share/locale/* /usr/share/man /usr/share/doc /lib/xtables/libip6* /root/.cache
