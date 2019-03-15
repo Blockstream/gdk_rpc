2c283fcea1f696fab944388b93fc174adcd5a263
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-15 02:13:41 +0100

    wait for bitcoin core to be ready and generate 102 blocks before connecting to it

--- test.sh
+++ test.sh
@@ -3,6 +3,8 @@ set -eo pipefail
 
 /bitcoin/bin/bitcoind -server=1 -regtest -daemon
 
+/bitcoin/bin/bitcoin-cli -regtest -rpcwait generate 102
+
 cargo test --all
 
 gcc -o test test.c -Isrc  -L. -l:target/debug/libgdk_rpc.so && ./test
