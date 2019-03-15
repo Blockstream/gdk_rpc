a14f69ed5c2f282e8a492ece4534032df181cebb
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-15 01:32:58 +0100

    run bitcoind in regtest mode for tests

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+/bitcoin/bin/bitcoind -server=1 -regtest -daemon
+
+
+cargo test --all
+
 gcc -o test test.c -Isrc  -L. -l:target/debug/libgdk_rpc.so && ./test
+
+/bitcoin/bin/bitcoin-cli -regtest stop
