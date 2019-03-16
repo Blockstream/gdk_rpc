c90a5acacc9f87bbb00882d4706daa29e79018ee
Nadav Ivgi <nadav@shesek.info>
2019-03-16 17:02:09 +0200

    Run cargo fmt

--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,5 @@
 extern crate libc;
-#[cfg(feature="stderr_logger")]
+#[cfg(feature = "stderr_logger")]
 extern crate stderrlog;
 #[macro_use]
 extern crate serde_json;
@@ -136,7 +136,7 @@ lazy_static! {
 
 #[test]
 fn a0_setup() {
-    #[cfg(feature="stderr_logger")]
+    #[cfg(feature = "stderr_logger")]
     stderrlog::new().verbosity(3).init().unwrap();
 }
 
