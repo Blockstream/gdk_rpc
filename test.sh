04f8e9b56469d969fc37fea56aab570a2de1253e
Lawrence Nahum <lawrence@greenaddress.it>
2019-03-16 14:50:43 +0100

    use bitcoind and bitcoin-cli from path, if docker add daemons to path

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -eo pipefail
 
-/bitcoin/bin/bitcoind -server=1 -regtest -daemon
+if [ -f /.dockerenv ]; then
+    export PATH=${PATH}:/bitcoin/bin:/liquid/bin
+fi
 
-/bitcoin/bin/bitcoin-cli -regtest -rpcwait generate 102
+bitcoind -server=1 -regtest -daemon
+
+bitcoin-cli -regtest -rpcwait generate 102
 
 cargo test --all  -- --test-threads=1
 
 gcc -o test test.c -Isrc  -L. -l:target/release/libgdk_rpc.so && ./test
 
-/bitcoin/bin/bitcoin-cli -regtest stop
+bitcoin-cli -regtest stop
